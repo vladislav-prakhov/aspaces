ffe90763638583181cef8dcca87c010869938cae
Vladislav Prakhov <vladislav.prakhov@fintechru.org>
2022-10-01 14:47:50 +0300

    chore: clearing initial project config

---
+++ front/src/App/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aspaces Application</h1>
+      <aside>
+      </aside>
+      <header>
+        header here
+      </header>
+    </div>
+  );
+}
+
+export default App;
