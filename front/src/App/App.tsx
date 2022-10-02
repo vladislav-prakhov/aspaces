222296539538815a1810910652f18b845c004a26
Vladislav Prakhov <vladislav.prakhov@fintechru.org>
2022-10-02 14:17:13 +0300

    chore: add basic layout

--- front/src/App/App.tsx
+++ front/src/App/App.tsx
@@ -1,14 +1,24 @@
 import './App.css';
+import { Button } from '@mui/material'
+import { UserButton } from '../components';
 
 function App() {
   return (
     <div className="App">
-      <h1>Aspaces Application</h1>
-      <aside>
-      </aside>
-      <header>
-        header here
-      </header>
+        <aside>
+          <UserButton name='user 1'/>
+          <UserButton name='user 2'/>
+          <UserButton name='user 3'/>
+        </aside>
+        <body>
+            <header>
+              header here
+            </header>
+            <div className='content'>
+              <h1>Aspaces Application</h1>
+              <Button variant="text">Just click</Button>
+            </div>
+        </body>
     </div>
   );
 }
